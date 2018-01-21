61a34ecdbb808bb6d7c6b6f3fecff70fcc3b9936
Lukas Behrendt <lukas.behrendt@student.hpi.de>
2018-01-21 17:04:42 +0100

    Results so far

---
+++ parse_xml.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+from bs4 import BeautifulSoup
+import csv
+import re
+
+
+def parsetags(tagstring):
+    return re.findall("<(.*?)>", tagstring)
+
+
+s = ""
+with open('data.xml', 'r') as f:
+    s = f.read()
+
+soup = BeautifulSoup(s, "lxml")
+rows = soup.posts.findAll("row")
+
+authors = set()
+posts = {}
+tags = {}
+for row in rows:
+    author = row.get("owneruserid", None) or row["ownerdisplayname"]
+    authors.add(author)
+    pid = row["id"]
+    tags[pid] = parsetags(row.get("tags", ""))
+    posts[pid] = author
+
+edges = []
+for row in rows:
+    if row.get("parentid", None):
+        author = row.get("owneruserid", None) or row["ownerdisplayname"]
+        parent_id = row["parentid"]
+        parent_author = posts[parent_id]
+        edges.append([author, parent_author, *tags[parent_id]])
+
+with open('nodes.csv', 'w') as f:
+    w = csv.writer(f)
+    for pid, author in posts.items():
+        w.writerow([author])
+with open('edges.csv', 'w') as f:
+    w = csv.writer(f)
+    w.writerows(edges)
